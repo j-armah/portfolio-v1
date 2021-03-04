8542c8d8b9cd73f41c30daee3dcbd6323e13fc6b
februar-y <jarmah241@gmail.com>
2021-03-04 15:07:56 -0500

    initial commit, sets very basic frame

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Jeremy's Portfolio</h1>
+      <h2> About me </h2>
+      <h2> Contacts </h2>
+      <h2> Welcome </h2>
+      <h2> Projects </h2>
+    </div>
+  );
+}
+
+export default App;
