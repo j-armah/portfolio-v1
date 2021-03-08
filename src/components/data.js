9b018a76fe727223b14b8010182ffa4f21619b86
februar-y <jarmah241@gmail.com>
2021-03-08 14:26:14 -0500

    fixes typo

--- src/components/data.js
+++ src/components/data.js
@@ -2,17 +2,17 @@ const projects = {
     flixfix : {
         name: "FlixFix",
         url : "https://flixfix.netlify.app/",
-        technologies = ["Javascript", "Rails"]
+        technologies : ["Javascript", "Rails"]
     },
     gamesrx : {
         name: "GamesRx",
         url: "https://gamesrx.netlify.app/",
-        technologies = ["React", "Rails"]
+        technologies : ["React", "Rails"]
     },
     lfg: {
         name: "LFG",
         url: "https://lfg-beta.netlify.app/",
-        technologies = ["React", "Rails"]
+        technologies : ["React", "Rails"]
     }
 }
 
